998e3cdac0e4c9f107f359b1b633ee33b796a6a5
Parker Young <parker@parkedonrails.com>
2013-05-09 14:05:00 -0500

    Updating things a bit, shouldn't break anything. Upgrading version to 0.0.2

--- lib/n_gram.rb
+++ lib/n_gram.rb
@@ -1,49 +1,59 @@
 class NGram
-  def initialize(data, options={})
-    @ns = options[:n].is_a?(Array) ? options[:n] : [options[:n]]
-    @data = data
-    
-    @bucket_holder = []
-    @all_ngram = template_hash
-    
+  attr_accessor :ns, :data, :by_input, :all_data
+  def initialize(source_data, options={n:1})
+    default_values(source_data, options)
     process
   end
-  
-  # Provides an ngram for each item in the data array
-  def ngrams_of_inputs
-    @bucket_holder
+
+  def ngrams_of_inputs(ngram_count = nil)
+    re_process(ngram_count)
+    by_input
   end
-  
-  # Provides an ngram of all the data
-  def ngrams_of_all_data
-    @all_ngram
+
+  def ngrams_of_all_data(ngram_count = nil)
+    re_process(ngram_count)
+    all_data
   end
-  
+
   private
-  
+
+  def default_values(source_data = self.data, options = {n:1})
+    self.ns = [options[:n]].flatten
+    self.data = source_data
+    self.by_input = []
+    self.all_data = template_hash
+  end
+
+  def re_process(ngram_count)
+    if !ngram_count.nil?
+      default_values(self.data, n: ngram_count)
+      process
+    end
+  end
+
   def process
-    @data.each do |string|
+    data.each do |string|
       bucket = template_hash
-      
+
       words = string.split(' ')
-      
+
       (words.size).times do |index|
-        @ns.each do |n|
+        self.ns.each do |n|
           # Next if we've ran out of words
           next if (index + n) > words.size
           bucket[n][words[index,n].join(' ')] += 1
-          @all_ngram[n][words[index,n].join(' ')] += 1
+          all_data[n][words[index,n].join(' ')] += 1
         end
       end
-      
-      @bucket_holder << bucket
+
+      by_input << bucket
     end
   end
-  
+
   def template_hash
-    Hash.new do |hash, key| 
+    Hash.new do |hash, key|
       hash[key] = Hash.new {|hash, key| hash[key] = 0}
     end
   end
-  
+
 end
